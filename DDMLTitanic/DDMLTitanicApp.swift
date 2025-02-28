b43ccfe89450572030333291a6cd8c3d91e91872
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-02-28 23:35:14 +0900

    Initial Commit

---
+++ DDMLTitanic/DDMLTitanicApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDMLTitanicApp.swift
+//  DDMLTitanic
+//
+//  Created by joe on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DDMLTitanicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
